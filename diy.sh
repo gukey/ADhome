ea253354f776b75c20ad2ce7ddbb2759748c4223
gukey <378738159@qq.com>
2020-03-28 15:27:49 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -10,8 +10,12 @@ sed -i 's/192.168.1.1/192.168.1.3/g' package/base-files/files/bin/config_generat
 sed -i 's/bootstrap/argon/g' feeds/luci/collections/luci/Makefile
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 git clone https://github.com/gukey/smartdns.git package/openwrt-smartdns
-git clone https://github.com/vernesong/OpenClash.git package/OpenClash
-#git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome  
+#git clone https://github.com/vernesong/OpenClash.git package/OpenClash
+#git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome 
+
+
+#LUCI 添加
+#  // CONFIG_PACKAGE_luci-theme-argon=y
 #  //CONFIG_PACKAGE_luci-app-adguardhome=y
 ./scripts/feeds update -a
 ./scripts/feeds install -a
